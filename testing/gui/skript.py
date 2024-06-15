0803f39b66f9956c74530b6dc7a389f86c4f68ec
MauriceMoest <155661552+MauriceMoest@users.noreply.github.com>
2024-06-15 15:37:51 +0200

    GUI Interface

--- testing/gui/skript.py
+++ testing/gui/skript.py
@@ -1,19 +1,19 @@
 import time
 
-from video_landmarks import FaceLandmarkDetector, EyeColorDetector, FaceDetector, GlassesDetector 
+from video_landmarks import FaceLandmarkDetector, EyeColorDetector, GlassesDetector, AgeGenderDetector
 
-predictor_path = "../Utils/shape_predictor_68_face_landmarks.dat"
+predictor_path = "C:/Users/Mauri/Desktop/BV3-WEB3_Projekt/testing/Utils/shape_predictor_68_face_landmarks.dat"
 
 
 def get_screenshot():
     detector = FaceLandmarkDetector(predictor_path)
-    image = detector.take_screenshot("screenshot.jpg")
+    image = detector.take_screenshot("C:/Users/Mauri/Desktop/BV3-WEB3_Projekt/screenshot.jpg")
     return image
 
 
 def eye_color() -> str:
     eye_color_detector = EyeColorDetector(predictor_path)
-    result_eye_color = eye_color_detector.detect_eye_color("screenshot.jpg")
+    result_eye_color = eye_color_detector.detect_eye_color("C:/Users/Mauri/Desktop/BV3-WEB3_Projekt/screenshot.jpg")
     return result_eye_color
 
 
@@ -31,18 +31,21 @@ def facial_hair_color() -> str:
 
 def glasses() -> str:
     glasses_detector = GlassesDetector(predictor_path)
-    result_glasses = glasses_detector.display_results("screenshot.jpg")
+    result_glasses = glasses_detector.display_results("C:/Users/Mauri/Desktop/BV3-WEB3_Projekt/screenshot.jpg")
     return result_glasses
 
 
 def age() -> str:
-    return "25 - 30"
-
+    age_gender_detector = AgeGenderDetector("C:/Users/Mauri/Desktop/BV3-WEB3_Projekt/screenshot.jpg")
+    age_gender_detector.genger_age_detection()    
+    return age_gender_detector.age
 
 def race() -> str:
     return "European"
 
 
 def gender() -> str:
-    return "Female"
+    age_gender_detector = AgeGenderDetector("C:/Users/Mauri/Desktop/BV3-WEB3_Projekt/screenshot.jpg")
+    age_gender_detector.genger_age_detection()    
+    return age_gender_detector.gender
 
